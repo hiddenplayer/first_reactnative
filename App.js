727f3ee9931e355b7f1c888b6edf12fe9530ad0a
hiddenplayer <ngocthien.dnt@gmail.com>
2022-01-12 22:09:22 +0700

    hi

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Task } from "./components/Task";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWraper}>
+        <Text style={styles.title}>Today's tasks</Text>
+        <View style={styles.items}>{<Task text="aafef" />}</View>
+        <View style={styles.items}>{<Task text="aafef" />}</View>
+        <View style={styles.items}>{<Task text="aafef" />}</View>
+      </View>
+
+      <View style={styles.addTask}>
+        <TextInput
+          placeholder="Write a task"
+          style={styles.inputTask}
+        ></TextInput>
+        <View style={styles.plus}>
+          <Text>+</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f4f4f4",
+    paddingHorizontal: 20,
+    paddingTop: 50,
+  },
+  tasksWraper: {},
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingBottom: 30,
+  },
+  items: {
+    marginTop: 5,
+  },
+  addTask: {
+    position: "absolute",
+    bottom: 60,
+    width: "90%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  inputTask: {
+    backgroundColor: "#fff",
+    flex: 0.95,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    borderRadius: 60,
+  },
+  plus: {
+    backgroundColor: "#fff",
+    width: 50,
+    height: 50,
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
---
+++ components/Task.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const Task = (props) => {
+  return (
+    <View style={styles.taskItem}>
+      <View style={styles.itemLeft}>
+        <View style={styles.square}></View>
+        <Text style={styles.txt}>{props.text}</Text>
+      </View>
+      <View style={styles.itemRight}></View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  taskItem: {
+    backgroundColor: "#fff",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 7,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  txt: {
+    fontSize: 18,
+  },
+  itemRight: {
+    borderColor: "#55BCF6",
+    borderRadius: 5,
+    borderWidth: 2,
+    width: 12,
+    height: 12,
+  },
+  square: {
+    backgroundColor: "#55BCF6",
+    borderRadius: 5,
+    padding: 10,
+    marginRight: 10,
+  },
+});
